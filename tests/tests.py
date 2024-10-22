cda513e41eb32cfe2c04d0bf826fbbbd12e519bc
StasYu <stasugai101@gmail.com>
2024-10-22 17:39:38 +0000

    task_1

---
+++ tests/tests.py
@@ -0,0 +1,17 @@
+import random
+from typing import List
+from src.task_1 import is_prime
+from src.task_1 import primes
+from src.task_1 import checksum
+from src.task_1 import pipeline
+
+
+
+def test_functions():
+    assert is_prime(13) == True
+    assert primes(3) == [2, 3, 5]
+    assert checksum([1, 2, 6, 24]) == 6012369
+    assert pipeline() == 7785816
+
+if __name__ == '__main__':
+    test_functions()
